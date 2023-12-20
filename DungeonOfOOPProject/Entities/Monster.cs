f8712e6a9b0e4b00496d93fa1384d8f351a8589b
Vlad Chirila <vlad.chirila@live.com>
2023-12-20 08:28:08 +0200

    make monster abstract

--- DungeonOfOOPProject/Entities/Monster.cs
+++ DungeonOfOOPProject/Entities/Monster.cs
@@ -1,6 +1,6 @@
 ﻿namespace DungeonOfOOPProject.Entities
 {
-    public class Monster
+    public abstract class Monster
     {
         public MonsterType Type { get; set; }
         public int Health { get; set; }
@@ -8,7 +8,7 @@
         public bool IsAlive { get; set; } = true;
         public string[] Messages { get; set; }
         public string Appearance { get; set; }
-        public virtual void Interact(Player player);
+        public abstract void Interact(Player player);
         public abstract void ShowIntroduction();
 
     }
